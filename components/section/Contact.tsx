2a9260bb71d1ce4a8f668b3d78230b4a98c648c1
Nathan <solomonnathan065@gmail.com>
2025-09-28 19:52:42 +0300

    footer update

--- components/section/Contact.tsx
+++ components/section/Contact.tsx
@@ -11,9 +11,21 @@ const Contact = () => {
                 height={60}
                 className='absolute top-16 md:top-10 left-10 z-30'
             />
-            <p className="text-xl text-center w-max  absolute right-10 top-16 md:top-10 z-10 max-w-4xl px-4 text-white font-[SuisseIntl-Regular] mb-4 rounded-full border-[1px] border-white">
-                Contact Us
-            </p>
+            <div className='absolute right-10 top-16 md:top-10 z-10 max-w-4xl'>
+                <p className="text-xl text-center w-max   px-4 text-white font-[SuisseIntl-Regular] mb-4 rounded-full border-[1px] border-white">
+                    Contact Us
+                </p>
+                <ul className='block lg:hidden'>
+                    <li className='text-white text-lg md:text-2xl font-[SuisseIntl-Light] mb-2 flex flex-col justify-end items-end'>
+                        <a href="https://www.facebook.com/hambaricho" target="_blank" rel="noopener noreferrer" className='underline ml-2'>Facebook</a>
+                        <a href="https://www.instagram.com/hambaricho" target="_blank" rel="noopener noreferrer" className='underline ml-2'>Instagram</a>
+                        <a href="https://www.linkedin.com/company/hambaricho" target="_blank" rel="noopener noreferrer" className='underline ml-2'>LinkedIn</a>
+                        <a href="https://twitter.com/hambaricho" target="_blank" rel="noopener noreferrer" className='underline ml-2'>Twitter</a>
+                        <a href="https://www.tiktok.com/@hambaricho" target="_blank" rel="noopener noreferrer" className='underline ml-2'>TikTok</a>
+                    </li>
+                </ul>
+
+            </div>
             <ContactPhysicsFooter />
         </div>
     )
